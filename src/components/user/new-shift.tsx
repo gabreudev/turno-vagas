f7e2bda0b4ac69c06a7fae6504c737a2c70a6761
gabreudev <biel0alves0@gmail.com>
2025-03-18 16:03:51 -0300

    feat: add checkbox

--- src/components/user/new-shift.tsx
+++ src/components/user/new-shift.tsx
@@ -1,9 +1,32 @@
+'use client';
+
+import { createShift } from '@/app/actions';
+import { USER_SHIFTS_CACHE_KEY } from '@/common/constants/cache';
+import { SHIFT_END_AT, SHIFT_START_AT } from '@/common/constants/schedule';
+import type { CreateShiftRequestDto } from '@/common/validations/shift/create-shift/create-shift-request.dto';
+import type { GetShiftsStringDatesResponseDto } from '@/common/validations/shift/get-shift/get-shifts-response.dto';
+import type { ShiftStringDatesDto } from '@/common/validations/shift/shift.dto';
+import {
+  shiftStatusSchema,
+  weekDaySchema,
+} from '@/common/validations/shift/shift.dto';
+import { Checkbox } from '@/components/ui/checkbox';
+import { useToast } from '@/hooks/use-toast';
+import { minutesToTimeString } from '@/utils/format-time';
+import { useQueryClient } from '@tanstack/react-query';
+import { Loader2 } from 'lucide-react';
+import type {
+  ControllerRenderProps,
+  FieldPath,
+  FieldValues,
+} from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { Button } from '../ui/button';
 import {
-  DialogHeader,
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogHeader,
   DialogTitle,
 } from '../ui/dialog';
 import { Label } from '../ui/label';
@@ -14,28 +37,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from '../ui/select';
-import type { ShiftStringDatesDto } from '@/common/validations/shift/shift.dto';
-import {
-  shiftStatusSchema,
-  weekDaySchema,
-} from '@/common/validations/shift/shift.dto';
-import type {
-  ControllerRenderProps,
-  FieldPath,
-  FieldValues,
-} from 'react-hook-form';
-import { Controller, useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import type { CreateShiftRequestDto } from '@/common/validations/shift/create-shift/create-shift-request.dto';
-import { createShiftRequestSchema } from '@/common/validations/shift/create-shift/create-shift-request.dto';
-import { Loader2 } from 'lucide-react';
-import { createShift } from '@/app/actions';
-import { SHIFT_END_AT, SHIFT_START_AT } from '@/common/constants/schedule';
-import { minutesToTimeString } from '@/utils/format-time';
-import { useQueryClient } from '@tanstack/react-query';
-import { USER_SHIFTS_CACHE_KEY } from '@/common/constants/cache';
-import type { GetShiftsStringDatesResponseDto } from '@/common/validations/shift/get-shift/get-shifts-response.dto';
-import { useToast } from '@/hooks/use-toast';
 
 interface NewShiftProps {
   userId: number;
@@ -95,13 +96,14 @@ export function NewShift({
     handleSubmit,
     control,
     formState: { isSubmitting, errors },
-  } = useForm<CreateShiftRequestDto>({
-    resolver: zodResolver(createShiftRequestSchema),
+    watch,
+  } = useForm<CreateShiftRequestDto & { termsAccepted: boolean }>({
     defaultValues: {
       userId,
       weekDay: 'SEGUNDA',
       startAt: 7 * 60,
       endAt: 22 * 60,
+      termsAccepted: false,
     },
   });
 
@@ -109,7 +111,8 @@ export function NewShift({
 
   const queryClient = useQueryClient();
 
-  async function onSubmit(newShiftData: CreateShiftRequestDto): Promise<void> {
+  async function onSubmit(formData: CreateShiftRequestDto & { termsAccepted: boolean }): Promise<void> {
+    const { termsAccepted, ...newShiftData } = formData
     const response = await createShift(newShiftData);
 
     if (!response.ok) {
@@ -272,7 +275,41 @@ export function NewShift({
               <p className="text-sm text-destructive">{errors.endAt.message}</p>
             )}
           </div>
+          <div className="flex items-center space-x-2 py-2">
+            <Controller
+              name="termsAccepted"
+              control={control}
+              render={({ field }) => (
+                <Checkbox
+                  id="termsAccepted"
+                  checked={field.value}
+                  onCheckedChange={(checked) =>
+                    field.onChange(checked === true)
+                  }
+                />
+              )}
+            />
+            <Label
+              htmlFor="termsAccepted"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Li e aceito os{' '}
+              <a
+                href="/termos.pdf"
+                download="termos.pdf"
+                className="cursor-pointer text-primary underline"
+              >
+                {' '}
+                termos e condições.
+              </a>
+            </Label>
+          </div>
 
+          {errors.termsAccepted && (
+            <p className="text-sm text-destructive">
+              {errors.termsAccepted.message}
+            </p>
+          )}
           <div className="flex flex-col gap-2 md:w-full md:flex-row">
             <Button
               variant="outline"
@@ -282,7 +319,11 @@ export function NewShift({
             >
               Cancelar
             </Button>
-            <Button type="submit" disabled={isSubmitting} className="w-full">
+            <Button
+              type="submit"
+              disabled={isSubmitting || !watch('termsAccepted')}
+              className="w-full"
+            >
               {isSubmitting ? (
                 <Loader2 className="h-5 w-5 animate-spin" />
               ) : (
